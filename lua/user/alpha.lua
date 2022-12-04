904b759a2e3f80c1f6870878653a6c86e3054be3
Maksim Zayakin <rasetsu@list.ru>
2022-12-04 10:39:56 +0500

    feat(ui): update header and footer

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -5,12 +5,26 @@ end
 
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = {
-	[[                               __                ]],
-	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+	[[                           ]],
+	[[                           ]],
+	[[                           ]],
+	[[       __                  ]],
+	[[      ___(  ~-/\___        ]],
+	[[     _)            ~"\     ]],
+	[[   _3    {~"\___      ~Z_  ]],
+	[[ =" x__/~"      ~""\ _  /  ]],
+	[[<__      /~-"/\     ~ ~y<  ]],
+	[[/___,  /" ---- ~\        \ ]],
+	[[   /_-C\  =P=    7       \ ]],
+	[[       P        /,-j    _\ ]],
+	[[      /         ~  /  /~   ]],
+	[[      ~\         -<  /     ]],
+	[[    ====@       ___/~      ]],
+	[[        {    _-{   ~"-,    ]],
+	[[         --r"/  |     }    ]],
+	[[    _______l/   |   ,-'--._ ]],
+	[[   ---,    | /~~ ,-"       ~\ ]],
+	[[                 ^       ]],
 }
 dashboard.section.buttons.val = {
 	dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
@@ -22,7 +36,7 @@ dashboard.section.buttons.val = {
 	dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 local function footer()
-	return "chrisatmachine.com"
+	return "whatever happens, happens"
 end
 
 dashboard.section.footer.val = footer()
